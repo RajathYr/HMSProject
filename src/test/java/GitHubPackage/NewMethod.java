862415407c91a2313d7d408f2ec85fb599b3e02c
Manju <Manju@DESKTOP-U2K6LOV>
2022-09-28 11:39:47 +0530

    m3 method

--- src/test/java/GitHubPackage/NewMethod.java
+++ src/test/java/GitHubPackage/NewMethod.java
@@ -8,8 +8,14 @@ public void m1()
 {
 	System.out.println("m1 method");
 	}
+@Test
 public void m2() 
 {
 	System.out.println("m2 method");
+	}
+@Test
+public void m3()
+{
+	System.out.println("m3 method");
 	}
 }
